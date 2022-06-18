1d4479e624274cf0e2c782083523ed5ff12129a3
Diego Sánchez <diegosandmg@gmail.com>
2022-06-17 21:04:56 -0400

    fix variable not being expanded

--- i3/.config/i3/setup_workspaces.sh
+++ i3/.config/i3/setup_workspaces.sh
@@ -3,17 +3,16 @@
 # Set wallpaper and generate colorscheme when i3 launches
 setwall
 
-bash $HOME/.config/polybar/launch.sh
+bash "$HOME/.config/polybar/launch.sh"
 
-i3-msg 'workspace 1; append_layout $HOME/.config/i3/workspace-1.json'
+i3-msg "workspace 1; append_layout $HOME/.config/i3/workspace-1.json"
 
 # Open google calendar window
-i3-msg 'workspace 1; exec gtk-launch $(xdg-settings get default-web-browser) https://calendar.google.com'
+i3-msg "workspace 1; exec gtk-launch $(xdg-settings get default-web-browser) https://calendar.google.com"
 
 # St terminal layout
 bash $HOME/.config/i3/setup_workspaces_2.sh
 
-i3-msg 'workspace 3; append_layout $HOME/.config/i3/workspace-3.json'
+i3-msg "workspace 3; append_layout $HOME/.config/i3/workspace-3.json"
 
-i3-msg 'workspace 3; exec --no-startup-id obsidian'
-
+i3-msg "workspace 3; exec --no-startup-id obsidian"
