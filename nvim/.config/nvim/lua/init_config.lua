f9acfdca1c0a70c13bbda1e60b0df64e7675c2a6
Diego Sánchez <diegosandmg@gmail.com>
2023-03-19 08:39:25 -0400

    use default capabilities

--- nvim/.config/nvim/lua/init_config.lua
+++ nvim/.config/nvim/lua/init_config.lua
@@ -62,7 +62,7 @@ require('nvim_comment').setup()
 
 --Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Mappings.
 local opts = { noremap=true, silent=true }
