902400a95b04f7709021af2538253be579a36614
Diego Sánchez <diegosandmg@gmail.com>
2023-07-22 07:35:19 -0400

    php language server

--- baseconfig/nvim/.config/nvim/lua/init_config.lua
+++ baseconfig/nvim/.config/nvim/lua/init_config.lua
@@ -167,3 +167,10 @@ cmp.setup {
   },
 }
 
+require'lspconfig'.phpactor.setup{
+    on_attach = on_attach,
+    init_options = {
+        ["language_server_phpstan.enabled"] = false,
+        ["language_server_psalm.enabled"] = false,
+    }
+}
