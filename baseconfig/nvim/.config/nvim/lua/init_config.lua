3d306409c928267a478b1b0dbb21e1d22abc40aa
Diego Sánchez <diegosandmg@gmail.com>
2023-09-17 08:22:05 -0400

    use vim.keymap instead of vim.api for lsp

--- baseconfig/nvim/.config/nvim/lua/init_config.lua
+++ baseconfig/nvim/.config/nvim/lua/init_config.lua
@@ -67,10 +67,12 @@ capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 -- Mappings.
 local opts = { noremap=true, silent=true }
 
-vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
