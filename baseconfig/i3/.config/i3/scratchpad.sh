2e78e60b13f90a8447ce806c5fa34e193deed581
Diego Sánchez <diegosandmg@gmail.com>
2023-10-01 09:46:28 -0400

    better scratchpad termianal layout with i3 workspaces

--- baseconfig/i3/.config/i3/scratchpad.sh
+++ baseconfig/i3/.config/i3/scratchpad.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Append layout in workspace 4
+i3-msg "workspace 4; append_layout  \"~/.config/i3/scratchpad-layout.json\""
+
 # Spawn a terminal
-command="exec alacritty --class scratchpad,scratchpad"
+i3-msg "workspace 4; exec --no-startup-id alacritty"
 
-i3-msg "$command"
+# Move container to scratchpad
+i3-msg "workspace 4; [con_mark=\"start_on_scratch\"] move scratchpad"
 
-# Move terminal to the scratchpad, wait one second, otherwise the "move" i3 command won't work
-sleep 1s && i3-msg "[class=\"scratchpad\"] move scratchpad"
+# Return back to previous workspace
+i3-msg "workspace back_and_forth"
