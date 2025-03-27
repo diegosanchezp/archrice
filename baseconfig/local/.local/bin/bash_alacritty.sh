0e6ee6406ce904bd4e767f78636d678040f327ed
Diego Sánchez <diegosandmg@gmail.com>
2025-03-27 16:35:35 -0400

    add support for alacritty titles
    As a cli parameter

--- baseconfig/local/.local/bin/bash_alacritty.sh
+++ baseconfig/local/.local/bin/bash_alacritty.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
-echo "Running $1"
-alacritty --option "env = { tesisdev = \"True\", tcommand = \"$*\" }"
+TITLE="tesis-no-title"
 
+opts=$(getopt -o ht: --long help,title: -n "$0" -- "$@")
+eval set -- "$opts"
+
+while true; do
+  case $1 in
+    -h|--help)
+      echo "Description: Spawn alacritty terminals that execute a command when spawned"
+      echo ""
+      echo "Usage: $0 [options] [args]"
+      echo ""
+      echo "Options:"
+      echo "  -h, --help        Show this help message"
+      echo "  -t, --title TITLE Set the title of the alacritty window"
+      exit 0
+      ;;
+    -t|--title)
+      TITLE="$2"
+      shift 2
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Invalid option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+alacritty --title "${TITLE:-Default Title}" --option "env = { tesisdev = \"True\", tcommand = \"$*\" }"
