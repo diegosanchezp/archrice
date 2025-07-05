53756c3c25a896cf46380b3ef7bc66ef9dd83722
Diego Sánchez <diegosandmg@gmail.com>
2025-07-05 09:40:51 -0400

    scratchpad is opened on workspace

--- baseconfig/i3/.config/i3/scratchpad.sh
+++ baseconfig/i3/.config/i3/scratchpad.sh
@@ -8,11 +8,8 @@ i3-msg "append_layout \"~/.config/i3/scratchpad-layout.json\"" && \
 # Move container to scratchpad
 i3-msg "[con_mark=\"start_on_scratch\"] move scratchpad" && \
 
-# Spawn a terminal with neovim
-setsid --fork alacritty --title "Notes" -e zsh -c "nvim -c ':colorscheme github_light_tritanopia' ~/Documents/ObsidianVaults/diegos-knowledge/rutinas-diarias/rutinas-diarias.md && exec zsh"
+# Spawn a terminal with neovim, when the file is opened, open a table of contents, this mimics <leader>sy keybinding
+setsid --fork alacritty --title "Notes" -e zsh -c "nvim -c ':colorscheme github_light_tritanopia' -c 'lua vim.defer_fn(function() vim.cmd(\"SymbolsOutline\"); vim.cmd(\"normal! zR\") end, 100)' ~/Documents/ObsidianVaults/diegos-knowledge/rutinas-diarias/rutinas-diarias.md && exec zsh"
 
 # Show the scratchpad
 i3-msg "scratchpad show"
-
-# Return back to previous workspace
-# i3-msg "workspace back_and_forth"
