f6d9ba25ff6d4e76f7ab8ebd7e7fae89ac937a41
Diego Sánchez <diegosandmg@gmail.com>
2025-07-04 21:30:11 -0400

    update ideapadconfig

--- baseconfig/i3/.config/i3/scratchpad.sh
+++ baseconfig/i3/.config/i3/scratchpad.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-# Append layout in workspace 4
-i3-msg "workspace 4; append_layout \"~/.config/i3/scratchpad-layout.json\"" &&
+WORKSPACE_NUM=1
+# Append layout in workspace 1
+i3-msg workspace $WORKSPACE_NUM && \
+i3-msg "append_layout \"~/.config/i3/scratchpad-layout.json\"" && \
 
 # Move container to scratchpad
-i3-msg "workspace 4; [con_mark=\"start_on_scratch\"] move scratchpad" && \
+i3-msg "[con_mark=\"start_on_scratch\"] move scratchpad" && \
 
 # Spawn a terminal with neovim
-i3-msg "workspace 4; exec bash ~/.config/i3/scratchpad_command.sh" &&
+setsid --fork alacritty --title "Notes" -e zsh -c "nvim -c ':colorscheme github_light_tritanopia' ~/Documents/ObsidianVaults/diegos-knowledge/rutinas-diarias/rutinas-diarias.md && exec zsh"
+
+# Show the scratchpad
+i3-msg "scratchpad show"
 
 # Return back to previous workspace
-i3-msg "workspace back_and_forth"
+# i3-msg "workspace back_and_forth"
--- baseconfig/i3/.config/i3/setup_workspaces_2.sh
+++ baseconfig/i3/.config/i3/setup_workspaces_2.sh
@@ -2,6 +2,6 @@
 i3-msg "workspace 2; append_layout $HOME/.config/i3/workspace-2.json"
 
 # Spawn 4 alacritty terminals
-for ((n=0;n<4;n++)); do
+for ((n=0;n<1;n++)); do
   i3-msg "workspace 2; exec alacritty";
 done
